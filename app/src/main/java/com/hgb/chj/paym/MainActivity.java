8a3440f44fa2348b739fc089926a041cc622dee4
chahj <alxious@naver.com>
2019-01-19 00:27:37 +0900

    2019-01-18

--- app/src/main/java/com/hgb/chj/paym/MainActivity.java
+++ app/src/main/java/com/hgb/chj/paym/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hgb.chj.paym;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button TestButton = (Button)findViewById(R.id.TestButton);
+
+        TestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent TestIntent = new Intent(MainActivity.this,PayMainActivity.class);
+                MainActivity.this.startActivity(TestIntent);
+            }
+        });
     }
 }
